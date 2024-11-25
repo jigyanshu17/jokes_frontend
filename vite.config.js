0be31c9300635fd88721136fb944dd4a3e9d94ff
Jigyanshu Don <jigyanshukumawat@gmail.com>
2024-11-25 22:25:20 +0530

    link vercel

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      "/api": "https://jokesbackend.vercel.app",
     },
   },
   plugins: [react()],
-})
+});
